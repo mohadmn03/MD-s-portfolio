10de0df04bfb35b2259ab8ed373515e0bc54a335
Moha Dmn <91742203+mohadmn03@users.noreply.github.com>
2022-04-25 03:17:26 +0100

    Add files via upload

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -22,4 +22,56 @@ window.addEventListener("scroll", (event) => {
     });
   }
 });
+//latest work
+//random images
+let demmaneBox = document.querySelector(".demmane img");
+let demmaneArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+let buddytreeBox = document.querySelector(".buddytree img");
+let buddytreeArray = [1, 2, 3, 4, 5, 6];
 //
+function random(myArray) {
+  let randomNum = Math.floor(Math.random() * myArray.length);
+  return randomNum;
+}
+//
+setInterval(() => {
+  let randomNumOne = random(demmaneArray);
+  demmaneBox.src = `../images/randomImgs/demmane/demmane${demmaneArray[randomNumOne]}.png`;
+  //
+  let randomNumTwo = random(buddytreeArray);
+  buddytreeBox.src = `../images/randomImgs/buddytree/buddytree${buddytreeArray[randomNumTwo]}.png`;
+}, 3000);
+//
+let footer = document.querySelector(".footer");
+footer.onclick = (e) => e.stopPropagation();
+let whatsapp = document.querySelector(".whatsapp");
+whatsapp.addEventListener("click", (event) => {
+  event.preventDefault();
+  //overlay
+  let overlay = document.createElement("div");
+  overlay.className = "foverlay";
+  footer.appendChild(overlay);
+  //number
+  let number = document.createElement("div");
+  number.className = "number";
+  footer.appendChild(number);
+  //
+  let wai = document.createElement("i");
+  let wan = document.createElement("span");
+  let wanText = document.createTextNode("+213665455773");
+  wan.appendChild(wanText);
+  wai.className = "fa-brands fa-whatsapp wi";
+  wan.className = "wn";
+  number.appendChild(wai);
+  number.appendChild(wan);
+  //
+  let close = document.createElement("div");
+  close.classList = "close";
+  close.innerHTML = "X";
+  number.appendChild(close);
+  //
+  close.addEventListener("click", (event) => {
+    overlay.remove();
+    number.remove();
+  });
+});
